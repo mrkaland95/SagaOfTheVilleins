000c44990d5677de954c84b987613919b80f32ea
Steffen-Admin <mrkaland95@gmail.com>
2023-02-03 20:17:01 +0100

    Work on 2d class. Not quite working but close.

--- core/src/com/project/roguelike/Animator2D.java
+++ core/src/com/project/roguelike/Animator2D.java
@@ -1,78 +1,48 @@
 package com.project.roguelike;
-import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
-public class Animator2D implements ApplicationListener {
+public class Animator2D {
     // Adjusted class from the official libgdx documentation, to implement 2d animations.
     // https://libgdx.com/wiki/graphics/2d/2d-animation
 
     // Defines the columns of the sprite sheet
     private static final int FRAME_COLUMNS = 12;
-
+    private static final int FRAME_ROWS = 1;
     Animation<TextureRegion> walkAnimation;
-    Texture warriorWalkSheet;
-    SpriteBatch spriteBatch;
+    Texture animationSheet;
 
     // Variable for tracking elapsed time for the animation.
     float elapsedAnimationTime;
 
-    @Override
-    public void create() {
-        warriorWalkSheet = new Texture(Gdx.files.internal("./Sprites/Warrior/WalkingWarrior.png"));
+    public Animator2D(String spriteSheet) {
+        this.animationSheet = new Texture(Gdx.files.internal(spriteSheet));
+        // Transforms the 2d texture array back into a 1d array
 
-        // Has to be done this way because the TextureRegion class expects a 2d array.
-        TextureRegion[][] tempTextures = TextureRegion.split(warriorWalkSheet, warriorWalkSheet.getWidth() / FRAME_COLUMNS, warriorWalkSheet.getHeight());
+        TextureRegion[][] tempTextures = TextureRegion.split(animationSheet,
+                animationSheet.getWidth() / FRAME_COLUMNS,
+                animationSheet.getHeight() / FRAME_ROWS
+        );
 
 
-        // Places the textures back into a 1d array in the correct order.
-        // Doing this beacuse the "Animation" constructor requires a 1D array
         TextureRegion[] walkFrames = new TextureRegion[FRAME_COLUMNS];
         int index = 0;
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < FRAME_ROWS; i++) {
             for (int j = 0; i < FRAME_COLUMNS; j++) {
                 walkFrames[index++] = tempTextures[i][j];
             }
         }
-
-        walkAnimation = new Animation<TextureRegion>(0.025f, walkFrames);
-        spriteBatch = new SpriteBatch();
+        this.walkAnimation = new Animation<TextureRegion>(0.025f, walkFrames);
         elapsedAnimationTime = 0f;
-    }
-
-    @Override
-    public void resize(int width, int height) {
 
     }
 
-    @Override
-    public void render() {
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT); // Clear screen
-        elapsedAnimationTime += Gdx.graphics.getDeltaTime();
-
+    public TextureRegion getImageToRender(float deltaTime) {
+        elapsedAnimationTime += deltaTime;
         TextureRegion currentFrame = walkAnimation.getKeyFrame(elapsedAnimationTime, true);
-        spriteBatch.begin();
-        spriteBatch.draw(currentFrame, 50, 50);
-        spriteBatch.end();
-    }
-
-    @Override
-    public void pause() {
-
-    }
-
-    @Override
-    public void resume() {
-
-    }
-
-    @Override
-    public void dispose() {
-        spriteBatch.dispose();
-        warriorWalkSheet.dispose();
+        return currentFrame;
     }
 }
+
--- core/src/com/project/roguelike/ProjectRogueLikeMain.java
+++ core/src/com/project/roguelike/ProjectRogueLikeMain.java
@@ -1,33 +1,38 @@
 package com.project.roguelike;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class ProjectRogueLikeMain extends ApplicationAdapter {
-	SpriteBatch batch;
+	SpriteBatch spriteBatch;
 	Texture img;
 
 	Animator2D walkingWarrior;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("./Sprites/Warrior/WalkingWarrior.png");
+		spriteBatch = new SpriteBatch();
+		walkingWarrior = new Animator2D("./Sprites/Warrior/WalkingWarrior.png");
 	}
 
 	@Override
 	public void render () {
+		float deltaTime = Gdx.graphics.getDeltaTime();
 		ScreenUtils.clear(1, 1, 1, 1);
-		batch.begin();
-		batch.draw(img, 0, 50);
-		batch.end();
+		spriteBatch.begin();
+		TextureRegion frame = walkingWarrior.getImageToRender(deltaTime);
+//		batch.draw(img, 0, 50);
+		spriteBatch.draw(frame, 50, 50);
+		spriteBatch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		spriteBatch.dispose();
 		img.dispose();
 	}
 }
