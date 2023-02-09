786c969f343b28a31adbdcb25cfa9bd3ca500d06
Markus <markus.sylta@student.uib.no>
2023-02-09 08:38:22 +0100

    Work on map

---
+++ assets/Green_Hex.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Green_Hex" tilewidth="200" tileheight="200" tilecount="1" columns="1">
+ <image source="green Hex2.png" width="200" height="200"/>
+</tileset>
--- core/src/com/project/roguelike/TestGameMain.java
+++ core/src/com/project/roguelike/TestGameMain.java
@@ -1,31 +1,88 @@
 package com.project.roguelike;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
-
-public class TestGameMain extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.HexagonalTiledMapRenderer;
+public class TestGameMain extends ApplicationAdapter implements InputProcessor{
 	
+	private TiledMap map;
+	private HexagonalTiledMapRenderer renderer;
+	private OrthographicCamera camera;
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		map = new TmxMapLoader().load("./TiledMap/TiledRougelikeMap.tmx");
+		renderer = new HexagonalTiledMapRenderer(map);
+		camera = new OrthographicCamera();
+		Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		renderer.setView(camera);
+		renderer.render();
+
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		map.dispose();
+		renderer.dispose();
+	}
+
+	@Override
+	public void resize(int width, int height){
+		camera.viewportHeight = height;
+		camera.viewportWidth = width;
+
+		camera.update();
+	}
+
+	@Override
+	public boolean keyDown(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(float amountX, float amountY) {
+		return false;
 	}
 }
