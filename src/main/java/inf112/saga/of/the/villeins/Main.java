bb5da9d140a70f6c346e99a9ff1b57bb53593436
Leon <pet017@uib.no>
2023-02-14 17:59:17 +0100

    args

--- src/main/java/inf112/saga/of/the/villeins/Main.java
+++ src/main/java/inf112/saga/of/the/villeins/Main.java
@@ -6,7 +6,7 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
 // Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
 public class Main {
-	public static void main (String[] arg) {
+	public static void main(String[] args) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(30);
 		config.setTitle("Saga Of The Villeins");
