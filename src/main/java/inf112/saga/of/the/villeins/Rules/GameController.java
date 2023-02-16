955abb249023a9678bab393917f315c7c0af99ef
Markus <markus.sylta@student.uib.no>
2023-02-16 10:01:55 +0100

    Inplemented GameController and IInputProcessor

--- src/main/java/inf112/saga/of/the/villeins/Rules/GameController.java
+++ src/main/java/inf112/saga/of/the/villeins/Rules/GameController.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 
 import inf112.saga.of.the.villeins.Characters.ICharacter;
 import inf112.saga.of.the.villeins.InputProcessors.CameraProcessor;
+import inf112.saga.of.the.villeins.InputProcessors.IInputProcessor;
 import inf112.saga.of.the.villeins.InputProcessors.PlayerProcessor;
 
 public class GameController {
@@ -15,11 +16,13 @@ public class GameController {
     private ArrayList<ICharacter> playerlist;
     private int turnCounter;
     private ArrayList<InputProcessor> processorList;
+    public IInputProcessor currentProcessor;
 
     public GameController(ArrayList<ICharacter> playerlist, OrthographicCamera camera){
         this.playerlist = playerlist;
         this.turnCounter = 0;
         this.processorList = new ArrayList<>();
+        this.currentProcessor = null;
 
         initializeProcessors(camera);
     }
@@ -38,6 +41,8 @@ public class GameController {
     public void nextTurn(){
         /*
          * Sets the turn to the next charcter in turnList
+         * Should also change which processor in use based on if its a playerturn or AI-turn
+         * Maybe also use turnConter if needed
          */
     }
 
@@ -48,12 +53,13 @@ public class GameController {
         */
 
     private void initializeProcessors(OrthographicCamera camera){
-        InputProcessor game = new CameraProcessor(camera);
-        InputProcessor player = new PlayerProcessor(camera);
+        IInputProcessor game = new CameraProcessor(camera);
+        IInputProcessor player = new PlayerProcessor(camera);
 
         processorList.add(game);
         processorList.add(player);
 
+        this.currentProcessor = game;
         Gdx.input.setInputProcessor(game);
     }
 }   
