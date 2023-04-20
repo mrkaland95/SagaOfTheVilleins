30c004f51aa1dcac358448c993c5a5778fb89f5b
Leon <pet017@uib.no>
2023-04-20 09:07:19 +0200

    ga watertiles en false boolean slik at vi kan bruke det til å hindre characthers åp gå over de

--- assets/Maps/waterTile.tsx
+++ assets/Maps/waterTile.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.0" name="waterTile" tilewidth="200" tileheight="200" tilecount="1" columns="1">
+ <properties>
+  <property name="isWater" type="bool" value="false"/>
+ </properties>
  <image source="Tiles/PNG/waterTile.png" width="200" height="200"/>
 </tileset>
